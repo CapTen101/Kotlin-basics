8bf2abc5c4f34a86f8c57c60b35de6c9b2806d47
Tushar Rohilla <tusharrohilla.iitr@gmail.com>
2020-08-23 23:52:08 +0530

    back to kotlin

---
+++ src/Kotlintut.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    println("Yo!")
+    println("hello world") // println for a new line
+
+    var x = 5
+    println("this is a $x string") // this is called string templates
+    println("this is ${x.div(4)}")
+
+    //declaring explicit variables
+    var y: Int = 3
+    val z: Int = 4 // val is the constant datatype whose value won't change
+}
