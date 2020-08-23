8464241dbdb5296062b4550a69782ef0a21347fc
Tushar Rohilla <tusharrohilla.iitr@gmail.com>
2020-08-24 00:27:19 +0530

    more kotlin

--- src/Kotlintut.kt
+++ src/Kotlintut.kt
@@ -9,4 +9,31 @@ fun main(args: Array<String>) {
     //declaring explicit variables
     var y: Int = 3
     val z: Int = 4 // val is the constant datatype whose value won't change
+
+    //now we'll see nullpointerexception handling
+    var myname: String? = null // ? means a null type variable
+
+    // Conditional Statements can return a datatype after execution
+    val a2m: Int = if (true) {
+        print("inside rtue")
+        10 // this 10 is returned
+    } else {
+        print("false")
+        0 // this is returned
+    }
+
+    val name = "Tushar"
+    // 'when' can also be used just like switch-case
+    when (name) {
+        "Tushar" -> {
+            println("correct")
+        }
+        else -> {
+            print("yoo else")
+        }
+    }
+
+    // arrayOf functionality
+    val names = arrayOf(1,2,3)
+
 }
